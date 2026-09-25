1bfaf54becc839ea86fef65e209652a0c661b7bd
Julianbello <julianbellotiven@gmail.com>
2026-09-25 15:17:58 -0500

    Mejora interfaz tipo MercadoLibre y correccion de endpoints API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/User");
+const User = require("../models/user");
 const connectDatabase = require("../config/database");
 
 function createToken(user) {
